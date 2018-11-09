bb5c1ed4744bd289c0980a06fad325554e1dc612
Hunter <hunterhrasmussen@gmail.com>
2018-11-08 23:18:32 -0700

    Finished the unbanded and made it so that it prints to table.  Can't figure out why the return values aren't getting placed into the right spots

--- proj4/GeneSequencing.py
+++ proj4/GeneSequencing.py
@@ -19,6 +19,10 @@ MATCH = -3
 INDEL = 5
 SUB = 1
 
+DIAGONAL = 0
+LEFT = 1
+UP = -1
+
 
 # class Matrix:
 #
@@ -39,7 +43,7 @@ class GeneSequencing:
 	def printList(self,results,sequence1,sequence2,seq1Length,seq2Length):
 		adjustedLength1 = seq1Length+1
 		adjustedLength2 = seq2Length+1
-		print('adj len 1:',adjustedLength1,' ad len 2:' , adjustedLength2)
+		##print('adj len 1:',adjustedLength1,' ad len 2:' , adjustedLength2)
 		#print out the letters of the first sequence along the top
 		print(end='- - ')
 		for u in range(seq1Length):
@@ -67,86 +71,125 @@ class GeneSequencing:
 
 
 
+		toReturn = []
+
 		#for each pair of sequences
 		for i in range(len(sequences)):
 			jresults = []
 			for j in range(len(sequences)):
-				print("looking at sequence ", i, " and sequence ", j)
-				if banded:
-					hi = 0
-				else:
-					sequence1 = sequences[i]
-					sequence2 = sequences[j]
-					seq1Length = len(sequences[i])
-					seq2Length = len(sequences[j])
-					adjustedLength1 = seq1Length + 1
-					adjustedLength2 = seq2Length + 1
-					#print('adjustedLength1 =' ,adjustedLength1,' adjusted length2 = ',adjustedLength2)
-					#sequence 1 is the columns/spans the top of the table
-					#sequence 2 is the row / spans the side of the table
-					#make all cells have a value 0
-					#results = [[0 for x in range(seq1Length+1)] for y in range(seq2Length+1)]
-					#pointers = [[0 for x in range(seq1Length+1)] for y in range(seq2Length+1)]
+				if i <= j:
+					#print("looking at sequence ", i, " and sequence ", j)
+					if banded:
+						hi = 0
+					else:
+						sequence1 = sequences[i]
+						sequence2 = sequences[j]
+						seq1Length = len(sequences[i])
+						seq2Length = len(sequences[j])
+						if(seq2Length > align_length):
+							seq2Length = align_length
+						if(seq1Length> align_length):
+							seq1Length = align_length
+						adjustedLength1 = seq1Length + 1
+						adjustedLength2 = seq2Length + 1
+						#although I am using only one list, I treat is as a matrix.
+						#after having gone the length of seq, the next 'row' begins
+						results = (seq2Length+1) * (seq1Length+1) * [math.inf]
+						pointers = (seq2Length+1) * (seq1Length+1) * [math.inf]
+						#print('results len = ', len(results))
+						cost = 0
+						#populate 1st row with multiples of five
+						#print('populating 1st row')
+						for k in range(seq1Length+1):
+							results[k] = cost
+							cost+=5
 
-
-					#columns = [None]*(seq1Length+1)
-					#results = [columns]*(seq2Length+1)
-
-					#although I am using only one list, I treat is as a matrix.
-					#after having gone the length of seq, the next 'row' begins
-					results = (seq2Length+1) * (seq1Length+1) * [math.inf]
-					#print('results len = ', len(results))
-					cost = 0
-					#populate 1st row with multiples of five
-					print('populating 1st row')
-					for k in range(seq1Length+1):
-						results[k] = cost
-						cost+=5
-
-					cost = 5
-					#populate 1st column with multiples of 5
-					#start at the beginning of the second row
-					print('populating 1st column')
-					index = seq1Length+1
-					for k in range(seq2Length):
-						#print('line 96 index = ', index)
-						results[index] = cost
-						cost+=5
-						#move to the next "row"
-						index+=(seq1Length+1)
+						cost = 5
+						#populate 1st column with multiples of 5
+						#start at the beginning of the second row
+						#print('populating 1st column')
+						index = seq1Length+1
+						for k in range(seq2Length):
+							#print('line 96 index = ', index)
+							results[index] = cost
+							cost+=5
+							#move to the next "row"
+							index+=(seq1Length+1)
 
 
 
-					#calculate the cost for the rest
-					for l in range(1,adjustedLength2):			#  'rows'
-						for m in range(1,adjustedLength1):		#  'columns'
-							index = (l*adjustedLength1)+m
-							#print('index = ', index)
-							left = (results[index-1])+5
-							up = (results[index - adjustedLength1])+5
-							diagonal = 200
-							#if the letters match
-							previousdiagIndex = index - adjustedLength1 - 1
-							if(sequence2[l-1] == sequence1[m-1]):
-								diagonal = results[previousdiagIndex] - 3
-							else:
-								diagonal = results[previousdiagIndex] + 1
-							# find the min
-							#diagonal preceeds up preceeds left
-							if diagonal < left and diagonal < up:
-								results[index] = diagonal
-							elif up < diagonal and up < left:
-								results[index] = up
-							else:
-								results[index] = left
+						#calculate the cost for the rest
+						for l in range(1,adjustedLength2):			#  'rows'
+							for m in range(1,adjustedLength1):		#  'columns'
+								index = (l*adjustedLength1)+m
+								#print('index = ', index)
+								left = (results[index-1])+5
+								up = (results[index - adjustedLength1])+5
+								diagonal = 200
+								#if the letters match
+								previousdiagIndex = index - adjustedLength1 - 1
+								if(sequence2[l-1] == sequence1[m-1]):
+									diagonal = results[previousdiagIndex] - 3
+								else:
+									diagonal = results[previousdiagIndex] + 1
+								# find the min
+								#diagonal preceeds up preceeds left
+								if diagonal < left and diagonal < up:
+									results[index] = diagonal
+									pointers[index] = DIAGONAL
+								elif up < diagonal and up < left:
+									results[index] = up
+									pointers[index] = UP
+								else:
+									results[index] = left
+									pointers[index] = LEFT
 
 
 
-					if i < 2 and j < 2:
-						self.printList(results,sequence1,sequence2,seq1Length,seq2Length)
+						if i < 2 and j < 2:
+							self.printList(results,sequence1,sequence2,seq1Length,seq2Length)
 
-				if(j < i):
-					s = {}
+					score = results[((seq2Length+1) * (seq1Length+1))-1]
+					table.item(i,j).setText('{}'.format(int(score)))
+					table.repaint()
+					index = ((seq2Length+1) * (seq1Length+1))-1
+					#backtrace
+					aligment1 = []
+					aligment2 = []
+					seq1Index = seq1Length - 1
+					seq2Index = seq2Length -1
+					while(index!=0):
+						temp = pointers[index]
+						if(seq1Index < 0):
+								seq1Index = 0
+						if (temp == DIAGONAL):
+							aligment1.append(sequence1[seq1Index])
+							seq1Index = seq1Index - 1
+							aligment2.append(sequence2[seq2Index])
+							seq2Index -=1
+							index -= (adjustedLength1 +1)
+
+						elif(temp == UP):
+							aligment2.append(sequence2[seq2Index])
+							aligment1.append('-')
+							seq2Index -=1
+							index -= adjustedLength1
+						else:
+							aligment1.append(sequence1[seq1Index])
+							aligment2.append('-')
+							seq1Index = seq1Index - 1
+							index -= 1
+					string1 = ''.join(reversed(aligment1))
+					string2 = ''.join(reversed(aligment2))
+					if(j == 9 and i == 2):
+						print(string1)
+						print(string2)
+					s = {'align_cost':score, 'seqi_first100':string1, 'seqj_first100':string2}
+					jresults.append(s)
+					print('length of jresults = ', len(jresults))
+				toReturn.append(jresults)
+		return toReturn
+
 				#else:
 ###################################################################################################
 # your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
@@ -202,7 +245,6 @@ class GeneSequencing:
 
 
 
-		#---------------		Print out the first 4 arrays ---------------------#
 
 
 # previousRow = adjustedLength1 * [None]			# save the previous row for quicker lookup
